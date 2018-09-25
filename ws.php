91600f5d8f30cf3115cfec9f7ed84e19bef765c6
Pedro Salamoni <salamonip@gmail.com>
2018-09-25 17:02:22 -0400

    tt

--- ws.php
+++ ws.php
@@ -5,7 +5,7 @@ $db   = "archII";
 $user = "archII";
 $pass = "rootroot";
 // conecta ao banco de dados
-$con = mysql_pconnect($host, $user, $pass) or trigger_error(mysql_error(),E_USER_ERROR); 
+$con = mysql_connect($host, $user, $pass) or trigger_error(mysql_error(),E_USER_ERROR); 
 // seleciona a base de dados em que vamos trabalhar
 mysql_select_db($db, $con);
 // cria a instrução SQL que vai selecionar os dados
