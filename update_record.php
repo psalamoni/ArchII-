871c385efe949a51b56c9f12088d3136a33dd8df
psalamoni <salamonip@gmail.com>
2018-09-30 23:30:34 -0400

    Update update_record.php

--- update_record.php
+++ update_record.php
@@ -9,3 +9,10 @@ $sql = "UPDATE archII_table SET record".$game."=".$record." WHERE id=".$id;
 $result = $conn->query($sql);
 $conn->close();
 ?>
+
+<html><body>
+<span class="about-stat"><b>49,830</b> subscribers</span>
+<span class="about-stat"> &bull; <b>6,560,832</b> views</span>
+<br>
+<span class="about-stat">Joined May 14, 2009</span>
+</body></html>
