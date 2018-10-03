de0f32d476a1811f833c223e9cb37f27c3a62115
Pedro Salamoni <salamonip@gmail.com>
2018-10-02 23:39:09 -0400

    xx

--- update_record.php
+++ update_record.php
@@ -5,14 +5,13 @@ $id = $_GET['id'];
 $game = $_GET['game'];
 $record  = $_GET['record'];
 
-$sql = "UPDATE archII_table SET record".$game."=".$record." WHERE id=".$id;
-$result = $conn->query($sql);
+$ratual1 = "SELECT record".$game." FROM users WHERE id=".$id;
+$ratual2 = $conn->query($ratual1);
+
+if ($ratual2['record'.$game] <= $record) {
+	$sql = "UPDATE users SET record".$game."=".$record." WHERE id=".$id;
+	$result = $conn->query($sql);
+}
+
 $conn->close();
 ?>
-
-<html><body>
-<span class="about-stat"><b>49,830</b> subscribers</span>
-<span class="about-stat"> &bull; <b>6,560,832</b> views</span>
-<br>
-<span class="about-stat">Joined May 14, 2009</span>
-</body></html>
